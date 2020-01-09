e8936e82b76b233112c4717be0b5422542f1326d
Bassmalla Hafez <36860931+bassmalla@users.noreply.github.com>
2020-01-09 11:48:28 +0200

    Create index.vuln1.js

---
+++ index.vuln1.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/vuln', Vulnerability);
+app.get('/vuln2', Vulnerability2);
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+const Vulnerability = (req, res) => {
+    var user = req.params.user;
+    var respond = `
+        <h1>Hi, ${user}</h1>
+    `
+    res.send(respond);
+}
+
+
+const Vulnerability2 = (req, res) => {
+    var {user} = req.params;
+    var respond = `
+        <script> var x = "${user}" </script>
+    `
+    res.send(respond);
+}
+let PASSWORD = '12345'
